7ec613ffc5479d1a5af1876dfbcc12932cc2eb2c
Jordan <jordan.janevski11@gmail.com>
2021-10-16 03:18:49 +0200

    first commit

---
+++ src/Bointsoft/Logger.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+
+namespace Bointsoft;
+
+
+class Logger
+{
+  
+    public function __construct()
+    {
+      
+    }
+    public function log()
+    {
+      
+    }
+
+  
+}
